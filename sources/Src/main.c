63b58c9fd70f6fca42eca547e36e58813b8a3cf1
Sasha7b9 <Sasha7b9@tut.by>
2020-10-12 10:14:16 +0300

    Windows пишет не удаось найти драйвер

--- sources/Src/main.c
+++ sources/Src/main.c
@@ -114,39 +114,64 @@ int main(void)
   */
 static void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+ 
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
 
-  /* Enable Power Control clock */
-  __HAL_RCC_PWR_CLK_ENABLE();
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    
+    /* The voltage scaling allows optimizing the power 
 
-  /* The voltage scaling allows optimizing the power consumption when the
-   * device is clocked below the maximum system frequency, to update the
-   * voltage scaling value regarding system frequency refer to product
-   * datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+consumption when the device is 
+       clocked below the maximum system frequency, to update 
 
-  /* Configure RCC Oscillators: All parameters can be changed according to
-   * user’s needs */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+the voltage scaling value 
+       regarding system frequency refer to product datasheet.  
 
-  /* RCC Clocks: All parameters can be changed according to user’s needs */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
-                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+*/
+    __HAL_PWR_VOLTAGESCALING_CONFIG
 
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+(PWR_REGULATOR_VOLTAGE_SCALE2);
+    
+    /* Enable HSI Oscillator and activate PLL with HSI as 
+
+source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 0x10;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+       int i = 0;
+       //      ERROR_HANDLER();
+    }
+    
+    /* Select PLL as system clock source and configure the 
+
+HCLK, PCLK1 and PCLK2 
+       clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | 
+
+RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | 
+
+RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+    if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, 
+
+FLASH_LATENCY_2) != HAL_OK)
+    {
+       int i = 0;
+//      ERROR_HANDLER();
+    }
 }
 
 /**
