cbf4d307ca4e59606e447f12f08e3239a7f72cb5
Sasha7b9 <Sasha7b9@tut.by>
2020-10-19 15:32:39 +0300

    *

--- sources/S8-53/S8-53/src/Hardware/Hardware.cpp
+++ sources/S8-53/S8-53/src/Hardware/Hardware.cpp
@@ -1,9 +1,5 @@
 #include <stm32f207xx.h>
 #include <stm32f2xx_hal.h>
-#include <stm32f2xx_hal_tim.h>
-#include <stm32f2xx_hal_gpio.h>
-#include <stm32f2xx_hal_rcc.h>
-#include <stm32f2xx_hal_dac.h>
 #include "Hardware.h"
 #include "Log.h"
 
