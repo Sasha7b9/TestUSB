33f9262aa18fb5ba54c69cb3ce80e782c061ef60
Sasha7b9 <Sasha7b9@tut.by>
2020-10-19 15:41:47 +0300

    *

--- sources/S8-53/S8-53/src/Globals.h
+++ sources/S8-53/S8-53/src/Globals.h
@@ -9,12 +9,7 @@
 #include <usbh_def.h>
 
 
-extern USBH_HandleTypeDef handleUSBH;
 extern HCD_HandleTypeDef handleHCD;
-extern SPI_HandleTypeDef handleSPI;
-extern ADC_HandleTypeDef handleADC;
-extern DAC_HandleTypeDef handleDAC;
-
 
 #define CLIENT_LAN_IS_CONNECTED  (gBF.clientLANisConnected)
 #define CABLE_LAN_IS_CONNECTED   (gBF.cableLANisConnected)
