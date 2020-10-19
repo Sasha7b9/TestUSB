2db6d0fa47e92d29ecc43df5b4836a02d9a57dc6
Sasha7b9 <Sasha7b9@tut.by>
2020-10-19 15:34:29 +0300

    *

--- sources/S8-53/S8-53/src/libs/stm32f2xx_it.cpp
+++ sources/S8-53/S8-53/src/libs/stm32f2xx_it.cpp
@@ -69,37 +69,10 @@ void SysTick_Handler(void)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
-void ADC_IRQHandler(void)
-{
-//    HAL_ADC_IRQHandler(&handleADC);
-}
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/// This interrupt call soft NSS for spi (see Hardware::SPIforPanel.c::PanelInit() and HAL_GPIO_EXTI_Callback().
-void EXTI0_IRQHandler(void)
-{
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-}
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/// See Hardware::SPIforPanel.c::HAL_SPI_RxCpltCallback().
-void SPI1_IRQHandler(void)
-{
-//    HAL_SPI_IRQHandler(&handleSPI);
-}
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
 void OTG_HS_IRQHandler(void)
 {
     HAL_HCD_IRQHandler(&handleHCD);
 }
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void DMA1_Stream5_IRQHandler()
-{
-    HAL_DMA_IRQHandler(handleDAC.DMA_Handle1);
-}
-
 
 #ifdef __cplusplus
 }
